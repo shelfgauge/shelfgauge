979133511f0b53ddda06b3392fc0f9d2aaf24500
Benjamin Feng <contact@fengb.me>
2017-06-16 17:53:05 -0500

    Pull in koa-views

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,8 +2,7 @@ import { Context } from "koa";
 import * as Router from "koa-router";
 
 import repo from "./repo";
-import { render } from "src/view";
 
 export default new Router()
-  .get("/", render("index.html"))
+  .get("/", (ctx: Context) => ctx.render("index"))
   .use("/repo", repo.routes(), repo.allowedMethods());
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,12 @@
 import * as Koa from "koa";
+import * as views from "koa-views";
+import * as APP_ROOT from "app-root-path";
 
 import proxy from "./proxy";
 import router from "src/router";
 
 export default new Koa()
+  .use(views(APP_ROOT + "/src/view"))
   .use(router.routes())
   .use(router.allowedMethods())
   .use(proxy);
