eb31780a75ab37a0581024de009232b00d63a458
Benjamin Feng <contact@fengb.me>
2017-06-16 23:35:25 -0500

    Use ENV.apiUrl for ajax

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,11 +2,18 @@ import * as Koa from "koa";
 import * as views from "koa-views";
 import * as APP_ROOT from "app-root-path";
 
+import ENV from "config/env";
+
 import proxy from "./proxy";
 import router from "src/router";
 
 export default new Koa()
-  .use(views(APP_ROOT + "/src/view"))
+  .use(
+    views(APP_ROOT + "/src/view", {
+      options: { API_URL: ENV.apiUrl },
+      map: { html: "lodash" }
+    })
+  )
   .use(router.routes())
   .use(router.allowedMethods())
   .use(proxy);
