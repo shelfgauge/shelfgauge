8ec6dd812e1ab1a56dc7a2ce4fc0f77a2877fae0
Benjamin Feng <contact@fengb.me>
2017-06-27 17:34:46 -0500

    Added error logging

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,7 @@
 import * as Koa from "koa";
 import * as views from "koa-views";
 import * as APP_ROOT from "app-root-path";
+const error = require("koa-error");
 
 import ENV from "config/env";
 
@@ -8,6 +9,7 @@ import proxy from "./proxy";
 import router from "src/router";
 
 export default new Koa()
+  .use(error())
   .use(
     views(APP_ROOT + "/src/view", {
       options: { API_URL: ENV.apiUrl },
