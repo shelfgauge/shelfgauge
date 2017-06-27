57b58454084744d07dbe0e0e740fbfe9d1511dbd
Benjamin Feng <contact@fengb.me>
2017-06-27 18:28:02 -0500

    Render list of lines

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,5 +5,11 @@ import repo from "./repo";
 
 export default new Router()
   .get("/", (ctx: Context) => ctx.render("index"))
-  .get("/chart", (ctx: Context) => ctx.render("chart.svg"))
+  .get("/chart", async (ctx: Context) => {
+    await ctx.render("chart.svg", {
+      width: 100,
+      height: 100,
+      lines: [[[20, 100], [40, 60], [70, 80], [100, 20]]]
+    });
+  })
   .use("/repo", repo.routes(), repo.allowedMethods());
